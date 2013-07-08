e5888af189b19ff6e67e8fcd5035e575040fc818
Jan Erik Svendsen <jan.erik.svendsen.1971@gmail.com>
2013-07-08 14:05:56 +0000

    read file index.html

--- web.js
+++ web.js
@@ -4,10 +4,14 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+   response.send("1");
    fs.readFileSync("index.html", function(err, data) {
+      response.send("2");
       if(err) throw err;
+      response.send("3");
       response.send(data.toString("utf-8"));
    });  
+   response.send("4");
 //response.send('Hello World 2!');
 });
 
